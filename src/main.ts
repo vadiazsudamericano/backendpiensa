d13e8269bbf8f448bde6a4626e7324146edd144d
vadiazsudamericano <vadiaz@sudamericano.edu.ec>
2025-07-13 17:26:02 -0500

    Nuevos cambios3

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
-// RUTA: src/main.ts (BACKEND)
+// RUTA: [Tu Proyecto de Backend]/src/main.ts
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // --- AÑADE ESTA LÍNEA ---
+  // --- ESTE ES EL BLOQUE IMPORTANTE ---
+  // Habilitamos CORS para permitir peticiones desde otros dominios.
   app.enableCors({
-    origin: 'https://medicleanfrontend.vercel.app/', // ¡Usa tu URL real de Vercel!
+    // Para la demo, puedes usar 'true' que permite cualquier origen.
+    // Para máxima seguridad en producción, deberías usar la URL de tu Vercel.
+    origin: 'https://medicleanfrontend.vercel.app', // Alternativa más segura
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
+  // Escucha en el puerto que le asigne Railway, o en el 3000 si es local.
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
